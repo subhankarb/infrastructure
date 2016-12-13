51dc5c015abe920861effe3b78d7c668c145ff5c
Chris Horsley <chris.horsley@csirtfoundry.com>
2016-12-13 12:01:57 +0100

    Remove env vars set from here, should be preset based on run

---
+++ scripts/run_cg_pipeline.sh
@@ -0,0 +1,11 @@
+set -e
+
+bash aws_init.sh
+tmpfile=$(mktemp) && $(cat container_def.json | tr '\n' ' ' | envsubst > $tmpfile) && cat $tmpfile && aws ecs register-task-definition --family $CYBERGREEN_AWS_ECS_TASK_FAMILY --cli-input-json file://$tmpfile
+bash dockerup.sh
+# returns the latest task revision we just created in ecs register-task-definition
+ecs_task_name=$(aws ecs list-task-definitions --family CYBERGREEN_AWS_ECS_TASK_FAMILY | jq --raw-output ".taskDefinitionArns[-1]")
+
+: "${ecs_task_name?Couldn\'t get ECS task name in $CYBERGREEN_AWS_TASK_FAMILY family from AWS, aborting}" 
+
+python3.5 -mbin.aws_task_queuer --cluster=$CYBERGREEN_AWS_ECS_CLUSTER --task='${ecs_task_name}' --max_tasks=2 openntp/201605*
