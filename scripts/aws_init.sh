620feea270f1700430c6b6238eb7dec4e1e1d1d9
Chris Horsley <chris.horsley@csirtfoundry.com>
2016-12-14 00:46:20 +0100

    Update scripts for better AWS checks, proper pathing

--- scripts/aws_init.sh
+++ scripts/aws_init.sh
@@ -6,7 +6,12 @@ set -e
 : "${CYBERGREEN_AWS_CLUSTER_NAME?Need to set CYBERGREEN_AWS_CLUSTER_NAME}" 
 : "${CYBERGREEN_AWS_REGION?Need to set CYBERGREEN_AWS_REGION}" 
 
+echo "Creating ECS cluster ${CYBERGREEN_AWS_CLUSTER_NAME}"
 aws ecs create-cluster --cluster-name ${CYBERGREEN_AWS_CLUSTER_NAME}
-# bucket we use for private data
-aws s3api create-bucket --bucket ${CYBERGREEN_S3_BUCKET_NAME} --acl private --create-bucket-configuration LocationConstraint=${CYBERGREEN_AWS_REGION} --region=${CYBERGREEN_AWS_REGION}
 
+echo "Creating bucket ${CYBERGREEN_S3_BUCKET_NAME}"
+# bucket we use for private data - may already exist, usually errors so || true
+(aws s3api create-bucket --bucket ${CYBERGREEN_S3_BUCKET_NAME} --acl private --create-bucket-configuration LocationConstraint=${CYBERGREEN_AWS_REGION} --region=${CYBERGREEN_AWS_REGION} || true)
+echo "Bucket create: $?"
+aws s3api get-bucket-acl --bucket ${CYBERGREEN_S3_BUCKET_NAME}
+echo "AWS setup complete"
--- scripts/run_cg_pipeline.sh
+++ scripts/run_cg_pipeline.sh
@@ -1,10 +1,18 @@
+#!/usr/bin/env bash 
+
 set -e
 
-bash aws_init.sh
-bash dockerup.sh
+SUBMODULE_DIR="submodules"
+SCRIPTS_DIR="scripts"
+ETL_DIR="${SUBMODULE_DIR}/etl2"
+
+bash ${SCRIPTS_DIR}/aws_init.sh
+bash "${ETL_DIR}/dockerup.sh"
 # returns the latest task revision we just created in ecs register-task-definition
 ecs_task_name=$(aws ecs list-task-definitions --family CYBERGREEN_AWS_ECS_TASK_FAMILY | jq --raw-output ".taskDefinitionArns[-1]")
 
 : "${ecs_task_name?Couldn\'t get ECS task name in $CYBERGREEN_AWS_TASK_FAMILY family from AWS, aborting}" 
 
+cd $ETL_DIR
 python3.5 -mbin.aws_task_queuer --cluster=$CYBERGREEN_AWS_ECS_CLUSTER --task='${ecs_task_name}' --max_tasks=2 openntp/201605*
+cd -
