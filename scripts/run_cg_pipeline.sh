9cbada485f23f316f402561d6c6b73e86fb498eb
Chris Horsley <chris.horsley@csirtfoundry.com>
2016-12-14 01:32:51 +0100

    Prep for installing venvs on sub projects

--- scripts/run_cg_pipeline.sh
+++ scripts/run_cg_pipeline.sh
@@ -5,6 +5,7 @@ set -e
 SUBMODULE_DIR="submodules"
 SCRIPTS_DIR="scripts"
 ETL_DIR="${SUBMODULE_DIR}/etl2"
+ETL_VENV_DIR="venv"
 
 bash ${SCRIPTS_DIR}/aws_init.sh
 bash "${ETL_DIR}/dockerup.sh"
@@ -13,6 +14,18 @@ ecs_task_name=$(aws ecs list-task-definitions --family CYBERGREEN_AWS_ECS_TASK_F
 
 : "${ecs_task_name?Couldn\'t get ECS task name in $CYBERGREEN_AWS_TASK_FAMILY family from AWS, aborting}" 
 
+# ETL
+
+if [ ! -d "$ETL_VENV_DIR" ]; then
+    virtualenv --python=python3 $ETL_VENV_DIR
+    echo "Virtualenv created at $ETL_VENV_DIR"
+fi
+
+source $ETL_VENV_DIR/bin/activate
+pip install --upgrade pip
+pip install $ETL_VENV_DIR/requirements.txt
+
 cd $ETL_DIR
 python3.5 -mbin.aws_task_queuer --cluster=$CYBERGREEN_AWS_ECS_CLUSTER --task='${ecs_task_name}' --max_tasks=2 openntp/201605*
 cd -
+deactivate
